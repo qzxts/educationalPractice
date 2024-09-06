3ffefcf239a64d678b2e673f54d9c8addd75e12f
Timur Semennikov <qzxts@outlook.com>
2024-09-06 20:27:42 +0800

    Made "decrypt()" function

--- Project.cpp
+++ Project.cpp
@@ -6,6 +6,56 @@ using namespace std;
 
 bool isFileOpen{ false }, isFileEmpty{ true };
 
+void decrypt(string textFileName) {
+    float freqCountInText[26];
+    string initialText, decryptedText, alphabet{ "ABCDEFGHIJKLMNOPQRSTUVWXYZ" };
+    ifstream inTextFile(textFileName);
+    getline(inTextFile, initialText);
+    decryptedText = initialText;
+    float freqCount;
+    unsigned short shiftValue, temp{ 0 };
+    if (inTextFile.is_open()) {
+        isFileOpen = true;
+        if ((inTextFile.peek() == std::ifstream::traits_type::eof()) == false || initialText.length() > 1) {
+            isFileEmpty = false;
+            for (unsigned short i = 0; i < 26; ++i) {
+                freqCount = 0;
+                for (unsigned short j = 0; j < decryptedText.length(); ++j) {
+                    if (static_cast<int>(decryptedText[j]) >= 97 && static_cast<int>(decryptedText[j]) <= 122) {
+                        decryptedText[j] = toupper(decryptedText[j]);
+                    }
+                    if (decryptedText[j] == alphabet[i]) {
+                        freqCount = freqCount + 1;
+                    }
+                }
+                freqCount = (freqCount * 100) / decryptedText.length();
+                freqCountInText[i] = freqCount;
+            }
+            for (unsigned short i = 0; i < 26; ++i) {
+                if (freqCountInText[i] > freqCountInText[0]) {
+                    freqCountInText[0] = freqCountInText[i];
+                    temp = i;
+                }
+            }
+            shiftValue = abs(4 - temp);
+            for (unsigned short i = 0; i < decryptedText.length(); ++i) {
+                if (isalpha(decryptedText[i])) {
+                    decryptedText[i] = ((decryptedText[i] - shiftValue - 'A' + 26) % 26) + 'A';
+                    if (static_cast<int>(initialText[i]) >= 97 && static_cast<int>(initialText[i]) <= 122) {
+                        decryptedText[i] = tolower(decryptedText[i]);
+                    }
+                }
+            }
+            inTextFile.close();
+            ofstream outTextFile(textFileName);
+            if (outTextFile.is_open()) {
+                outTextFile << decryptedText;
+                outTextFile.close();
+            }
+        }
+    }
+}
+
 int main() {
     string textFileName;
     unsigned short operationSelection;
